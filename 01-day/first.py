c629bfb5f374b14e5dde14310e84e9e4ff1593c7
Azizbekdevn1 <azizbekmurodov2003@gmail.com>
2024-12-01 17:02:11 +0500

    add new function

--- 01-day/first.py
+++ 01-day/first.py
@@ -1,5 +1,6 @@
+
 with open('input', 'r') as file:
-    lines = file.readlines()  # Har bir qatorni o'qiydi
+    lines = file.readlines()
 
 list1 = []
 list2 = []
@@ -10,13 +11,25 @@ for line in lines:
     list2.append(int(values[1]))
 
 
+# Second stars problem
+def calculate_similarity(left_list, right_list):
+    similarity_score = 0
+    for num in left_list:
+        similarity_score += num * right_list.count(num)
+    return similarity_score
+
+
 def calculate_total_distance(left_list, right_list):
     left_list.sort()
     right_list.sort()
 
+    # Calculate total distance
     total_distance = 0
-    for num in left_list:
-        total_distance += num*right_list.count(num)
-
+    for i in range(len(left_list)):
+        total_distance += abs(left_list[i] - right_list[i])
     return total_distance
+
 print(calculate_total_distance(list2, list1))
+# 2031679
+print(calculate_similarity(list1, list2))
+# 19678534
